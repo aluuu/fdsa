f2bef748b231ae6e26f008a4fcdbe132eda64efb
aluuu <zloy.alu@gmail.com>
2014-03-26 00:10:34 +0600

    metadata fix

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject fdsa/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject fdsa/lein-template "0.1.0"
+  :description "(Web-)app template"
+  :url "https://github.com/aluuu/fdsa"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "https://github.com/aluuu/fdsa"})
